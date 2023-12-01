83e71b5beeb7b51a0ba6612f358778a76218816a
Pele Pan <pqq12138@outlook.com>
2023-12-01 23:14:25 +0800

    :sparkles: Add port availability check

--- GinDemo.go
+++ GinDemo.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"net"
+	"os"
+	"strconv"
 )
 
 type RequestsBody struct {
@@ -10,7 +14,34 @@ type RequestsBody struct {
 	BankNos []string `json:"bankNos"`
 }
 
+func checkPortAvailability(port int) bool {
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return false
+	}
+	defer listener.Close()
+	return true
+}
+
+func findAvailablePort(port int) (int, error) {
+	for startPort := port + 1; startPort < 65535; startPort++ {
+		fmt.Println("checking port: ", startPort)
+		if checkPortAvailability(startPort) {
+			return startPort, nil
+		}
+	}
+
+	return 0, fmt.Errorf("no available port")
+}
+
 func main() {
+	port := 8080
+	availPort, err := findAvailablePort(port)
+	if err != nil {
+		fmt.Println("Failed to find available port")
+		os.Exit(1)
+	}
+
 	r := gin.Default()
 
 	r.GET("/getTest", func(c *gin.Context) {
@@ -62,8 +93,9 @@ func main() {
 
 	})
 
-	err := r.Run()
-	if err != nil {
+	startErr := r.Run(":" + strconv.Itoa(availPort))
+	if startErr != nil {
+		fmt.Println("Failed to start server")
 		return
 	}
 }
